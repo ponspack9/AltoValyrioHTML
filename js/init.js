e45b3ef5498391b7964637e678c67052665f3eb5
Iria Cervello <iria.cervello23@gmail.com>
2018-06-28 20:26:26 +0200

    No fa drop  down

--- js/init.js
+++ js/init.js
@@ -1,8 +1,13 @@
 (function($){
+  document.addEventListener('DOMContentLoaded',
+  function() {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems, options);
+  });
   $(function(){
 
     $('.sidenav').sidenav();
 
   }); // end of document ready
-  
+
 })(jQuery); // end of jQuery name space
