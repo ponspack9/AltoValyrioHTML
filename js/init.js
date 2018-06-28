22ddc4d767753e51422022556f8e2b4bb2265365
Iria Cervello <iria.cervello23@gmail.com>
2018-06-28 17:00:23 +0200

    try translate not work

--- js/init.js
+++ js/init.js
@@ -4,4 +4,8 @@
     $('.sidenav').sidenav();
 
   }); // end of document ready
+  function Translate(){
+    word=$('textarea1').val();
+    $('textarea2lbl').val("word");
+  }
 })(jQuery); // end of jQuery name space
