e641066a2fee83e0adcde67394dfe136c07d14b7
Iria Cervello <iria.cervello23@gmail.com>
2018-06-28 20:41:28 +0200

    select dropdown

--- js/init.js
+++ js/init.js
@@ -1,8 +1,8 @@
 (function($){
-  document.addEventListener('DOMContentLoaded',
-  function() {
-  var elems = document.querySelectorAll('.dropdown-trigger');
-  var instances = M.Dropdown.init(elems, options);
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var options= ["Español","English","Català"];
+    var instances = M.FormSelect.init(elems, options);
   });
   $(function(){
 
